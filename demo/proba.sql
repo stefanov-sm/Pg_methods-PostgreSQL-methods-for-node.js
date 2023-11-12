c735e6d6513e89a4ffae4e25995a6bcb0d7416f7
Stefan Stefanov <stefanov.sm@abv.bg>
2023-11-12 18:39:24 +0200

    Update proba.sql

--- demo/proba.sql
+++ demo/proba.sql
@@ -1,21 +1,15 @@
-  -- STEFANVS·S·DIGITARIVS·PRIMVS·FECIT
-  -- This is a comment too
-  
-  -- This line must bang
-  
-  --! {"name": "the_first_method", "returns": "record"}
-  
+-- This is a comment
+
+--! {"name": "the_first_method", "returns": "record"}
 SELECT n num, format('As text: %s', n) txt
   from generate_series(1, 100, 1) s(n)
   where n = $1;
-  
-  --! {"name": "the_second_method", "returns": "value"}
-  
+
+--! {"name": "the_second_method", "returns": "value"}
 SELECT to_char(n, 'FMRN') rn
   from generate_series(1, $1, 1) s(n)
   where n = $2;
-  
-  --! {"name": "the_third_method", "returns": "recordset"}
-  
+
+--! {"name": "the_third_method", "returns": "recordset"}
 SELECT n, to_char(n, 'FMRN') rn
-  from generate_series(1, 10, 1) s(n);
+  from generate_series(1, $1, 1) s(n);
