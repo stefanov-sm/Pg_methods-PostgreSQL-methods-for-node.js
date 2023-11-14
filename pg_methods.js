871424e28b57207d4b5d8807ef55f15aa5102992
Stefan Stefanov <stefanov.sm@abv.bg>
2023-11-14 14:18:56 +0200

    Update pg_methods.js

--- pg_methods.js
+++ pg_methods.js
@@ -6,7 +6,7 @@
 const COMMENT_RX = /^--[^!]|^$/, METHOD_RX = /^--!/, IDENT_RX = /^[_a-z]\w{0,62}$/i,
       RETURN_TYPE = ['recordset', 'record', 'value'], NEWLINE = '\n';
 
-function Pg_methods(pg_client, sql)
+function PgMethods(pg_client, sql)
 {
  this.sql_import = function(sql)
  {
@@ -21,24 +21,25 @@ function Pg_methods(pg_client, sql)
             let method_def = null;
             try {method_def = JSON.parse(line.substr(3))} catch (ignored) {};
 
-            if (!method_def
-                ||!(Object.keys(method_def).length == 2)
-                ||!('name' in method_def)
-                ||!('returns' in method_def)
-                ||!method_def.name.match(IDENT_RX)
-                ||!RETURN_TYPE.includes(method_def.returns))
+            if (!method_def || !(
+                Object.keys(method_def).length === 2
+                && 'name' in method_def
+                && 'returns' in method_def
+                && method_def.name.match(IDENT_RX)
+                && RETURN_TYPE.includes(method_def.returns)))
             {
                 throw new Error(`Method definition error, line ${line_number}: ${line}`);
             }
 
             method_name = method_def.name;
-            this[method_name] = {query_object: {name:method_name, text:'', values:[]}, returns:method_def.returns};
-            if (method_def.returns === 'value') this[method_name].query_object.rowMode = 'array';
+            if (method_def.returns !== 'value')
+            	this[method_name] = {returns:method_def.returns, query_object: {name:method_name, text:'', values:[]}};
+            else
+            	this[method_name] = {returns:method_def.returns, query_object: {name:method_name, text:'', values:[], rowMode: 'array'}};
 
             this[method_name].run = async function(...args)
             {
-                let query_object = {...this.query_object};
-                query_object.values = args;
+                let query_object = {...this.query_object, values: args};
                 const res = await pg_client.query(query_object);
                 switch (this.returns)
                 {
@@ -63,5 +64,5 @@ function Pg_methods(pg_client, sql)
 }
 module.exports =
 {
-    Pg_methods
+    PgMethods
 };
