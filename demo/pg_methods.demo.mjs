87703b10c0e26fb7a6aff0198cf7fa9cf2d6378e
Stefan Stefanov <stefanov.sm@abv.bg>
2023-11-14 17:43:56 +0200

    Update pg_methods.demo.mjs

--- demo/pg_methods.demo.mjs
+++ demo/pg_methods.demo.mjs
@@ -1,8 +1,7 @@
 const filename = 'proba.sql', another_filename = 'append.sql',
-      connectionString = 'postgresql://*****:*****@localhost:5432/postgres';
+	  connectionString = 'postgresql://postgres:Baba123Meca@localhost:5432/postgres';
 
-import pgmethods from './../pg_methods.js';
-
+import PgMethods from './../pg_methods.mjs';
 import postgresql from 'pg';
 import fs from 'fs';
 
@@ -11,18 +10,17 @@ await client.connect();
 
 try
 {
-    let db_gw = (new pgmethods.PgMethods(client))
-                .sql_import(fs.readFileSync(filename))
-                .sql_import(fs.readFileSync(another_filename));
-    
-    let res_a = await db_gw.the_first_method.run(10),
-        res_b = await db_gw.the_second_method.run(100, 26),
-        res_c = await db_gw.the_third_method.run(5),
-        res_d = await db_gw.alternative_method.run();
-    
-    console.log(res_a);
-    console.log(res_b);
-    console.log(res_c);
-    console.log(res_d);
+	let db_gw = (new PgMethods(client, fs.readFileSync(filename)))
+				.sql_import(fs.readFileSync(another_filename));
+	
+	let res_a = await db_gw.the_first_method.run(10),
+		res_b = await db_gw.the_second_method.run(100, 26),
+		res_c = await db_gw.the_third_method.run(5),
+		res_d = await db_gw.alternative_method.run();
+	
+	console.log(res_a);
+	console.log(res_b);
+	console.log(res_c);
+	console.log(res_d);
 }
 finally { await client.end(); } 
